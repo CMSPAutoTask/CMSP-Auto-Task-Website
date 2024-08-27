028e22425ecc9470bfdcda88c20f14869d21db49
CMSPAutoTask <cmspautotask@gmail.com>
2024-08-27 01:26:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+
 function register() {
     const user = document.getElementById('user_reg').value;
     const pw = document.getElementById('pw_reg').value;
@@ -15,3 +19,11 @@ function login() {
     
     window.location.href = url;
 }
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});

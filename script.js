9cc501509d9ebf33eb0710007d6d0f737cfab2b5
CMSPAutoTask <cmspautotask@gmail.com>
2024-08-31 05:12:39 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,46 @@
 const container = document.getElementById('container');
-const registerBtn = document.getElementById('registerBtn'); 
+const registerBtn = document.getElementById('registerBtn');
 const loginBtn = document.getElementById('loginBtn');
 const termsCheckBox = document.getElementById('terms');
 const createAccountBtn = document.getElementById('createAccountBtn');
 
-function register() {
-    const user = document.getElementById('user_reg').value;
-    const pw = document.getElementById('pw_reg').value;
+function submitForm(formId, action, apiUrl) {
+    grecaptcha.ready(function() {
+        grecaptcha.execute('6LcPKjMqAAAAACRFS-_zsvty2YIHUK0ylIY915wj', { action: action }).then(function(token) {
+            const form = document.getElementById(formId);
+            const user = document.getElementById(formId === 'registerForm' ? 'user_reg' : 'user_log').value;
+            const pw = document.getElementById(formId === 'registerForm' ? 'pw_reg' : 'pw_log').value;
 
-    fetch('https://cmsp-auto-task.vercel.app/api/register', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user: user,
-            pw: pw
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Aqui você pode adicionar lógica para tratar a resposta, como redirecionar ou mostrar uma mensagem
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        // Aqui você pode adicionar lógica para tratar erros
+            fetch(apiUrl, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: user,
+                    pw: pw,
+                    'g-recaptcha-response': token
+                })
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Aqui você pode adicionar lógica para tratar a resposta, como redirecionar ou mostrar uma mensagem
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                // Aqui você pode adicionar lógica para tratar erros
+            });
+        });
     });
 }
 
-function login() {
-    const user = document.getElementById('user_log').value;
-    const pw = document.getElementById('pw_log').value;
+function register() {
+    submitForm('registerForm', 'register', 'https://cmsp-auto-task.vercel.app/api/register');
+}
 
-    fetch('https://cmsp-auto-task.vercel.app/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user: user,
-            pw: pw
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-        // Aqui você pode adicionar lógica para tratar a resposta, como redirecionar ou mostrar uma mensagem
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        // Aqui você pode adicionar lógica para tratar erros
-    });
+function login() {
+    submitForm('loginForm', 'login', 'https://cmsp-auto-task.vercel.app/api/login');
 }
 
 function updateButtonState() {
@@ -68,5 +57,3 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
-
-
